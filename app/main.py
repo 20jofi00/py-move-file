0231d1fb7bc56b94b9c3d3a0dd120995364fd3fe
JoFi1977 <20jofi00@gmail.com>
2026-09-24 08:42:02 +0300

    done func move_file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3:
+        return
+    command_name, source_path, destination_path = parts
+    if command_name != "mv" or not os.path.isfile(source_path):
+        return
+    if destination_path.endswith("/"):
+        source_name = os.path.basename(source_path)
+        final_destination = os.path.join(destination_path, source_name)
+    else:
+        final_destination = destination_path
+
+    if os.path.abspath(source_path) == os.path.abspath(final_destination):
+        return
+
+    directory = os.path.dirname(final_destination)
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+
+    with open(source_path, "r") as file, open(final_destination, "w") as data:
+        content = file.read()
+        data.write(content)
+    os.remove(source_path)
